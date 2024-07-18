238298c3fbbec9acd5d3b70c22ccb3880d545eeb
mecrophagus <122439515+mecrophagus@users.noreply.github.com>
2024-07-18 19:37:12 +0000

    endpoints

---
+++ src/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+   # json_text = jsonify(todos)
+    return todos, 200
+
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    data = request.json
+    todos.append(data)
+    print('Incoming request ehit the following body', data)
+    print('todos', todos)
+    return todos
+
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    del todos[position]
+    print('This is the position to delete:', position)
+    return todos
+
+some_data = {"name": "Bobby", "lastname": "Rixer"}
+todos = [{"label": "My first task", "done": False}]
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
